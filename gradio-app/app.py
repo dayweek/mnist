a1da5bbd11a4c0ded8f3eb5d957a8fdef9ab7142
David Hrachovy <david@Sega.local>
2022-10-28 17:48:25 +0200

    Add flagging to hugging space dataset

--- gradio-app/app.py
+++ gradio-app/app.py
@@ -2,6 +2,12 @@ import torch
 import gradio as gr
 from torchvision import transforms
 from PIL import ImageOps
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hf_writer = gr.HuggingFaceDatasetSaver(os.getenv('HF_TOKEN'), "simple-mnist-flagging")
 
 def load_model():
     model_dict = torch.load('linear_model.pt')
@@ -11,20 +17,20 @@ model = load_model()
 convert_tensor = transforms.ToTensor()
 
 def predict(img):
-    img =  ImageOps.grayscale(img)
+    img =  ImageOps.grayscale(img).resize((28,28))
     image_tensor = convert_tensor(img).view(28*28)
     res = image_tensor @ model['weights'] + model['bias']
     res = res.sigmoid()
     return {"It's 3": float(res), "It's 7": float(1-res)}
 
 title = "Is it 7 or 3"
-description = '<p><center>Upload an image with a handwritten number: 7 or 3.</center></p>'
-examples = ['three.png', 'seven.png']
+description = '<p><center>Write a number, 7 or 3, in the middle.</center></p>'
 
 gr.Interface(fn=predict, 
-             inputs=gr.Image(type="pil"),
+             inputs=gr.Paint(type="pil", invert_colors=True),
              outputs=gr.Label(num_top_classes=2),
              title=title,
+             flagging_options=["incorrect","ambiguous"],
+             flagging_callback=hf_writer,
              description=description,
-             allow_flagging='never',
-             examples=examples).launch()
+             allow_flagging='manual').launch()
